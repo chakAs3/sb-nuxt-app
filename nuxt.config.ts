769f9284d3bd5f2d3839485e7b949cef4dca098d
chakAs3 <javachakir@gmail.com>
2024-03-29 15:17:08 +0400

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/image',
+    '@pinia/nuxt'
+  ],
+  runtimeConfig: {
+    app: {
+      name: 'Nuxt',
+      version: '1.0.0',
+      baseURL: '/',
+      host: 'localhost',
+      port: 3000,
+    },
+  },
+  pinia: {
+    autoImports: ['defineStore', 'acceptHMRUpdate'],
+  },
+  imports: {
+    dirs: ['./stores'],
+  },
+
+})
